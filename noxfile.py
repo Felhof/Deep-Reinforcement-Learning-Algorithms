7850ef173dc807231d6216ec95dc95cdf60ec29d
felix <felhof1@hotmail.com>
2022-01-28 16:31:12 +0100

    setup linting and type checking

---
+++ noxfile.py
@@ -0,0 +1,50 @@
+import tempfile
+from typing import Any
+
+import nox
+from nox.sessions import Session
+
+nox.options.sessions = "lint", "mypy"
+locations = "agents", "noxfile.py"
+
+
+@nox.session(python=["3.10"])
+def black(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session(python=["3.10"])
+def lint(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-annotations",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.10"])
+def mypy(session: Session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)

7c811aff9f528997b435a7efb98fde4dc486d0a4
felix <felhof1@hotmail.com>
2022-04-24 12:58:00 +0200

    change DQNBuffer to use Numpy Array instead of Deque

--- utilities/buffer/DQNBuffer.py
+++ utilities/buffer/DQNBuffer.py
@@ -1,30 +1,42 @@
-from collections import deque
-from typing import Deque, Tuple
+from typing import Tuple
 
 import numpy as np
 
 from utilities.config import Config
+from utilities.utils import get_dimension_format_string
 
 
 class DQNBuffer:
     def __init__(self: "DQNBuffer", config: Config, buffer_size: int) -> None:
         self.minibatch_size = config.hyperparameters["DQN"]["minibatch_size"]
         self.buffer_size = buffer_size
-        self.states: Deque[np.ndarray] = deque()
-        self.actions: Deque[np.ndarray] = deque()
-        self.rewards: Deque[float] = deque()
-        self.next_states: Deque[np.ndarray] = deque()
+        self.states = np.zeros(
+            self.buffer_size,
+            dtype=get_dimension_format_string(config.observation_dim),
+        )
+        self.actions = np.zeros(
+            self.buffer_size,
+            dtype=get_dimension_format_string(config.action_dim),
+        )
+        self.rewards = np.zeros(self.buffer_size, dtype=np.float32)
+        self.next_states = np.zeros(
+            self.buffer_size,
+            dtype=get_dimension_format_string(config.observation_dim),
+        )
+        self.index = 0
 
-    def add_transition(self, state: np.ndarray, action: np.ndarray, reward: float, next_state: np.ndarray) -> None:
-        if len(self.states) >= self.buffer_size:
-            self.states.popleft()
-            self.actions.popleft()
-            self.rewards.popleft()
-            self.next_states.popleft()
-        self.states.append(state)
-        self.actions.append(action)
-        self.rewards.append(reward)
-        self.next_states.append(next_state)
+    def add_transition(
+        self,
+        state: np.ndarray,
+        action: np.ndarray,
+        reward: float,
+        next_state: np.ndarray,
+    ) -> None:
+        self.states[self.index] = state
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.next_states[self.index] = next_state
+        self.index = (self.index + 1) % self.buffer_size
 
     def get_transition_data(
         self: "DQNBuffer",

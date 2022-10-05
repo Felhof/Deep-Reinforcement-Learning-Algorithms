2dff2c02ad5b722a25621e1d9ac22c8031fbd96c
felix <felhof1@hotmail.com>
2022-10-05 21:21:31 +0200

    make get_dimensional_format_string return 'dtype' instead of '1dtype' for scalar types

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,6 +1,8 @@
 def get_dimension_format_string(
     x_dim: int, y_dim: int = 1, dtype: str = "float32"
 ) -> str:
-    if y_dim == 1:
+    if y_dim > 1:
+        return f"({x_dim},{y_dim}){dtype}"
+    if x_dim > 1 and y_dim == 1:
         return f"{x_dim}{dtype}"
-    return f"({x_dim},{y_dim}){dtype}"
+    return dtype

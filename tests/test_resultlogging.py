daab300b4b7451ed8d71e80d0b08d09ef64805be
felix <felhof1@hotmail.com>
2022-10-07 16:45:38 +0200

    add support for logging tables to ResultLogger

--- tests/test_resultlogging.py
+++ tests/test_resultlogging.py
@@ -5,8 +5,9 @@ import pytest
 from utilities.resultlogging import LOG_DIRECTORY, ResultLogger
 
 
+TEST_LOGS_FILENAME = "test_logfile"
 PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-PATH_TO_TEST_LOGS = f"{PACKAGE_DIRECTORY}/../{LOG_DIRECTORY}/test_logfile.log"
+PATH_TO_TEST_LOGS = f"{PACKAGE_DIRECTORY}/../{LOG_DIRECTORY}/{TEST_LOGS_FILENAME}.log"
 
 
 @pytest.fixture
@@ -23,7 +24,7 @@ def test_logger_logs_to_console_by_default(capfd):
 
 
 def test_logger_logs_to_file_by_default(cleanup_test_logs):
-    logger = ResultLogger(filename="test_logfile")
+    logger = ResultLogger(filename=TEST_LOGS_FILENAME)
     logger.info("Hello World")
 
     logfile = Path(PATH_TO_TEST_LOGS)
@@ -33,3 +34,48 @@ def test_logger_logs_to_file_by_default(cleanup_test_logs):
         line = log.readline()
 
     assert "Hello World" in line
+
+
+def test_logger_logs_statistics_correctly(cleanup_test_logs):
+    logger = ResultLogger(filename=TEST_LOGS_FILENAME)
+
+    logger.store(scope="epoch", reward=1.0)
+    logger.store(scope="epoch", reward=2.0)
+    logger.store(scope="epoch", reward=3.0)
+
+    expected_table = [
+        "+-----------+--------+--------+--------+--------+",
+        "| attribute |  mean  |  std   |  max   |  min   |",
+        "+-----------+--------+--------+--------+--------+",
+        "|   reward  | 2.0000 | 0.8165 | 3.0000 | 1.0000 |",
+        "+-----------+--------+--------+--------+--------+",
+    ]
+
+    logger.log_table(scope="epoch", level="INFO")
+
+    logfile = Path(PATH_TO_TEST_LOGS)
+    assert logfile.is_file()
+
+    with open(PATH_TO_TEST_LOGS) as log:
+        lines = log.readlines()
+
+    assert expected_table[0] in lines[0]
+    assert expected_table[1] in lines[1]
+    assert expected_table[2] in lines[2]
+    assert expected_table[3] in lines[3]
+    assert expected_table[4] in lines[4]
+
+
+def test_logger_can_clear_data():
+    logger = ResultLogger()
+
+    logger.store(scope="episode", reward=1)
+    logger.store(scope="epoch", reward=2)
+    logger.store(scope="training", reward=3)
+
+    logger.clear(scope="episode")
+    logger.clear(scope="epoch")
+
+    assert logger.data["episode"]["reward"] == []
+    assert logger.data["epoch"]["reward"] == []
+    assert logger.data["training"]["reward"] == [3]
--- utilities/resultlogging.py
+++ utilities/resultlogging.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
 import logging
 import os
-from typing import Dict
+from typing import Dict, List
+
+import numpy as np
+from prettytable import PrettyTable
 
 
 PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
@@ -8,7 +12,7 @@ LOG_DIRECTORY = "logs"
 LOG_DIRECTORY_PATH = f"{PACKAGE_DIRECTORY}/../{LOG_DIRECTORY}"
 
 file_formatter = logging.Formatter(
-    "%(asctime)s %(levelname)s %(message)s -- module:%(module)s function:%(module)s"
+    "%(asctime)s %(levelname)s -- module:%(module)s function:%(module)s -- %(message)s "
 )
 console_formatter = logging.Formatter("%(levelname)s -- %(message)s")
 
@@ -51,3 +55,42 @@ class ResultLogger:
         self.error = logger.error
         self.critical = logger.critical
         self.logger = logger
+
+        self.data: Dict[str, defaultdict[str, List[float]]] = {
+            "episode": defaultdict(lambda: []),
+            "epoch": defaultdict(lambda: []),
+            "training": defaultdict(lambda: []),
+        }
+
+    def store(self: "ResultLogger", scope: str = "epoch", **kwargs) -> None:
+        for key, value in kwargs.items():
+            self.data[scope][key].append(value)
+
+    def clear(self: "ResultLogger", scope: str = "epoch") -> None:
+        self.data[scope] = defaultdict(lambda: [])
+
+    def log_table(
+        self: "ResultLogger", scope: str = "epoch", level: str = "INFO"
+    ) -> None:
+        table = PrettyTable()
+        table.field_names = ["attribute", "mean", "std", "max", "min"]
+
+        for attribute_name, attribute_data in self.data[scope].items():
+            mean = format(np.mean(attribute_data), ".4f")
+            std = format(np.std(attribute_data), ".4f")
+            attribute_max = format(max(attribute_data), ".4f")
+            attribute_min = format(min(attribute_data), ".4f")
+            table.add_row([attribute_name, mean, std, attribute_max, attribute_min])
+
+        log_function = {
+            "DEBUG": self.debug,
+            "INFO": self.info,
+            "WARN": self.warn,
+            "ERROR": self.error,
+            "CRITICAL": self.critical,
+        }[level]
+
+        table_strings = table.get_string().split("\n")
+
+        for table_string in table_strings:
+            log_function(table_string)

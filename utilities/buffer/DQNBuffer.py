Fix reward scaling for SAC and DQN

utilities/buffer/DQNBuffer.py
@@ -62,7 +62,7 @@ class DQNBuffer:
     def get_transition_data(
         self: "DQNBuffer",
     ) -> Dict[str, np.ndarray]:
-        weights = self.rewards[: self.number_of_stored_transitions]
+        weights = np.copy(self.rewards[: self.number_of_stored_transitions])
         if any(self.rewards < 0):
             reward_range = self.max_reward - self.min_reward
             if reward_range == 0:
@@ -72,9 +72,8 @@ class DQNBuffer:
                 weights += reward_range
 
         weights += (
-            np.arange(self.number_of_stored_transitions)
-            / self.number_of_stored_transitions
-        )
+            np.arange(self.number_of_stored_transitions) + 1
+        ) / self.number_of_stored_transitions
 
         weighted_probabilities = weights / sum(weights)
         transition_indices = np.random.choice(
@@ -82,10 +81,15 @@ class DQNBuffer:
             size=self.minibatch_size,
             p=weighted_probabilities,
         )
+        weighted_rewards = (
+            self.rewards[transition_indices] / sum(self.rewards[transition_indices])
+            if sum(self.rewards[transition_indices]) != 0
+            else self.rewards[transition_indices]
+        )
         return dict(
             states=self.states[transition_indices],
             actions=self.actions[transition_indices],
-            rewards=self.rewards[transition_indices],
+            rewards=weighted_rewards,
             next_states=self.next_states[transition_indices],
             done=self.done[transition_indices],
         )